0e7f7740f12b839ad9b26008b11c6146ec1e5014
amandohare13 <86521541+amandohare13@users.noreply.github.com>
2021-06-26 18:14:30 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1 class="note">Javascript and React.js</h1>
+      <p>A basic web dev Rect Js Bootcamp</p>
+    </div>
+  );
+}
+export default Info;
